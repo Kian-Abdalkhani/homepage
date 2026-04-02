3dfd6d9fd0d2a8050feb658cfe7cad179abc68c0
Kian.Abdalkhani <kabdalkhani@gmail.com>
2026-04-02 06:59:35 -0700

    added icons to /public and fix icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,13 @@
 import "./Header.css";
-import FamLogo from "../assets/icon.svg";
-import FamWaterMark from "/public/abdalkhani.svg";
 
 export function Header() {
   return (
     <header className="HeaderClass">
       <div className="header-logo">
-        <img src={FamLogo} alt="Family Logo" />
+        <img src="/icon.svg" alt="Family Logo" />
       </div>
       <div className="header-watermark">
-        <img src={FamWaterMark} alt="Family Watermark" />
+        <img src="/abdalkhani.svg" alt="Family Watermark" />
       </div>
       <div className="header-title">
         <p>Homepage</p>
